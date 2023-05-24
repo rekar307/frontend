ce4f34f70a19b6231dddaf30c9b23ce7f07575e9
rekar307 <rekar307@gmail.com>
2023-05-25 07:42:50 +0900

    refactoring getData from ajax
    Signed-off-by: rekar307 <rekar307@gmail.com>

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
 
-const newsFeed = JSON.parse(ajax.response)
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function() {
   const id = location.hash.substr(1);
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
 
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
   const title = document.createElement('h3');
 
   title.innerHTML = newsContent.title;
